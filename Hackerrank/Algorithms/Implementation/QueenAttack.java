e6a026ce868c7e35fb887271dd9a5edc2eb2829b
Enrique Medina <medina9304@gmail.com>
2019-07-21 13:55:03 -0400

    Completed queens attack

--- Hackerrank/Algorithms/Implementation/QueenAttack.java
+++ Hackerrank/Algorithms/Implementation/QueenAttack.java
@@ -10,7 +10,7 @@ package com.enriquemedina.codingchallenges.hackerrank.algorithms.implementation;
 public class QueensAttack {
 
 	public static void main(String[] args) {
-		//TODO
+		System.out.println(5,0,1,1,new int[][]);
 	}
 	
 	public static int mySolution(int boardSize, int nObstacles, int y, int x, int[][] obstacles) {
@@ -19,14 +19,14 @@ public class QueensAttack {
         distance[1] = boardSize - x;
         distance[2] = y-1;
         distance[3] = boardSize - y;
-        distance[4] = Math.min(x,y) - 1;
-        distance[5] = boardSize - Math.max(x,y);
-        distance[6] = Math.min(x,y) - 1;
-        distance[7] = boardSize - Math.max(x,y);
-
+        distance[4] = Math.min(distance[0],distance[2]);
+        distance[5] = Math.min(distance[0],distance[3]);
+        distance[6] = Math.min(distance[1],distance[2]);
+        distance[7] = Math.min(distance[1],distance[3]);
+        System.out.println("Initial distances are :" + Arrays.toString(distance));
 
        for(int[] obs : obstacles){
-
+           
            //Spots 0 and 1 will be x closeness
            if(obs[0] == y){
                if(obs[1] > x){
@@ -43,10 +43,31 @@ public class QueensAttack {
                 distance[2] = Math.min(distance[2], y-obs[0]-1);
                }
            }
+            // We know its diagonal 
+           if(Math.abs(y-obs[0]) == Math.abs(x-obs[1])){
+               System.out.println("Checking diagonal");
+               if(obs[0] < y && obs[1] < x){
+                   distance[4] = Math.min(distance[4], y-obs[0]-1);
+               }
+               if(obs[0] < y && obs[1] > x){
+                   distance[6] = Math.min(distance[6], y-obs[0]-1);
+               }
+               if(obs[0] > y && obs[1] < x){
+                   distance[5] = Math.min(distance[5], x-obs[1]-1);
+               }
+               if(obs[0] > y && obs[1] > x){
+                   distance[7] = Math.min(distance[7], obs[0]-y-1);
+               }
+           }
            
        }
+       int possibleMoves = 0 ;
+       for(int i : distance){
+           possibleMoves += i;
+       }
        System.out.println(Arrays.toString(distance));
-       return -1;
+       return possibleMoves;
     }
+
 	
 }
