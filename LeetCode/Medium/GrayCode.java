5ea08aae5150dace2e71ddae9ec3e70ac4c42187
Enrique Medina <medina9304@gmail.com>
2019-12-09 10:54:33 -0500

    LeetCode Medium: Non Optimum Gray Code

---
+++ LeetCode/Medium/GrayCode.java
@@ -0,0 +1,49 @@
+
+/**
+ * https://leetcode.com/problems/gray-code/
+ * Count all the numbers with unique digits 
+ * @Author EnMedina
+ * 
+ * Backtracking
+ */
+
+
+class Solution {
+  public List<Integer> grayCode(int n) {
+    List<Integer> currList = new LinkedList<>();
+    currList.add(0);
+    if( n == 0 ) return currList;
+
+    int maxNum = (int) (Math.pow(2, n));
+    boolean[] seen = new boolean[maxNum];   
+    seen[0] = true;
+    backtrack( currList,  seen, n );
+    return currList;
+  }
+
+  private void backtrack(List<Integer> currList, boolean[] seen, int n) {
+    if( currList.size() == seen.length) return; 
+    for( int i = 0 ; i < seen.length ; i++ ) {
+      if( !seen[i] && countDiffBits( currList.get(currList.size()-1), i ) == 1 ) {
+        seen[i] = true;
+        currList.add(i);
+        backtrack( currList, seen, n);
+        if( currList.size() == seen.length) return;
+        seen[i] = false;
+        currList.remove(currList.size()-1);
+      }
+    }
+    return;
+  }
+
+  private int countDiffBits(int  a, int b) {
+    int combination = a ^ b;
+    int count = 0; 
+    while (combination > 0) { 
+      combination &= (combination - 1); 
+      count++; 
+    } 
+    return count; 
+  }
+
+}
